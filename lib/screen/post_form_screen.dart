268b37658ed150b9ee1de60a4a6789b2d2f6e74b
AladdineDev <aaldn.dev@gmail.com>
2024-06-27 22:58:49 +0200

    feat: create screens

---
+++ lib/screen/post_form_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PostFormPage extends StatefulWidget {
+  const PostFormPage({super.key});
+
+  @override
+  State<PostFormPage> createState() => _PostFormPageState();
+}
+
+class _PostFormPageState extends State<PostFormPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
