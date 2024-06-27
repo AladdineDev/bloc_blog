b7ac33cdf9f120e14968fca8b88bf5ed1f180887
AladdineDev <aaldn.dev@gmail.com>
2024-06-28 00:15:54 +0200

    feat: add postId parameter to post detail screen

--- lib/screens/post_detail_screen.dart
+++ lib/screens/post_detail_screen.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 
 class PostDetailScreen extends StatelessWidget {
-  const PostDetailScreen({super.key});
+  const PostDetailScreen({
+    super.key,
+    required this.postId,
+  });
+
+  final String postId;
 
   @override
   Widget build(BuildContext context) {
