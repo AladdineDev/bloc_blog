2b3c66d988d532bb7f3c33b4a320dc36b265773e
AladdineDev <aaldn.dev@gmail.com>
2024-06-27 22:47:58 +0200

    feat: add navigator extensions

--- lib/extension/build_context_extension.dart
+++ lib/extension/build_context_extension.dart
@@ -1,5 +1,20 @@
 import 'package:flutter/material.dart';
 
-extension BuildContextExtension on BuildContext {
+extension ThemeDataExtension on BuildContext {
   ThemeData get theme => Theme.of(this);
+}
+
+extension NavigatorExtension on BuildContext {
+  NavigatorState get _navigator => Navigator.of(this);
+
+  Future<T?> pushNamed<T extends Object?>(
+    String routeName, {
+    Object? arguments,
+  }) {
+    return _navigator.pushNamed(routeName, arguments: arguments);
+  }
+
+  void pop() {
+    if (_navigator.canPop()) _navigator.pop();
+  }
 }
