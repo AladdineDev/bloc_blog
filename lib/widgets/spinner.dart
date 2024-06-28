5eff4ba91d6028792a591db75eade86d457f5885
AladdineDev <aaldn.dev@gmail.com>
2024-06-28 22:30:03 +0200

    feat: create custom circular progress indicator

---
+++ lib/widgets/spinner.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Spinner extends StatelessWidget {
+  const Spinner({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: SizedBox.square(
+        dimension: 32,
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
