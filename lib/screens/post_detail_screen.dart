d7515f7af41b21ec983a39f9d77469e05506a304
AladdineDev <aaldn.dev@gmail.com>
2024-06-29 20:32:15 +0200

    feat: display screen title

--- lib/screens/post_detail_screen.dart
+++ lib/screens/post_detail_screen.dart
@@ -13,6 +13,10 @@ class PostDetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: Center(
+        child: Text("PostDetailScreen"),
+      ),
+    );
   }
 }
