96973cd656f7fe2574804b7102a63a51af0e393a
AladdineDev <aaldn.dev@gmail.com>
2024-06-29 23:51:56 +0200

    feat: add exceptions

--- lib/exceptions/app_exception.dart
+++ lib/exceptions/app_exception.dart
@@ -15,10 +15,34 @@ class UnknownException extends AppException {
         );
 }
 
-class LoadPostsException extends AppException {
-  const LoadPostsException()
+class CreatePostException extends AppException {
+  const CreatePostException()
       : super(
-          'load-posts-exception',
+          'create-post-exception',
+          'Post publication has failed',
+        );
+}
+
+class ReadPostsException extends AppException {
+  const ReadPostsException()
+      : super(
+          'read-posts-exception',
           'Unable to load posts',
+        );
+}
+
+class ReadPostException extends AppException {
+  const ReadPostException()
+      : super(
+          'read-post-exception',
+          'Unable to load this post',
+        );
+}
+
+class UpdatePostException extends AppException {
+  const UpdatePostException()
+      : super(
+          'update-post-exception',
+          'Post update has failed',
         );
 }
