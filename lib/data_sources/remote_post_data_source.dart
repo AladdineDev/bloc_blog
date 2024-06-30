6c7f5269c026aae26510ee8be65621010e9f1d97
AladdineDev <aaldn.dev@gmail.com>
2024-06-30 13:43:47 +0200

    implement fake data source

--- lib/data_sources/remote_post_data_source.dart
+++ lib/data_sources/remote_post_data_source.dart
@@ -1,9 +1,27 @@
+import 'dart:math';
+
 import 'package:blog/data_sources/post_data_source.dart';
 import 'package:blog/exceptions/app_exception.dart';
 import 'package:blog/models/post.dart';
 
 class RemotePostDataSource extends PostDataSource {
+  RemotePostDataSource() {
+    _generatePosts(50);
+  }
+
   final List<Post> _posts = [];
+
+  void _generatePosts([int length = 20]) {
+    final random = Random();
+    for (int i = 0; i < length; i++) {
+      final post = Post(
+        id: '$i',
+        title: 'Post $i',
+        description: 'Bla bla, Blablabla bla.' * (random.nextInt(10) + 1),
+      );
+      _posts.add(post);
+    }
+  }
 
   @override
   Future<void> createPost({required Post post}) async {
