d770ad56d18d2f5e10c6a51c24296e792f490fa8
AladdineDev <aaldn.dev@gmail.com>
2024-06-30 22:29:23 +0200

    feat: implement copyWith in post model

--- lib/models/post.dart
+++ lib/models/post.dart
@@ -29,4 +29,16 @@ class Post extends Equatable {
       'description': description,
     };
   }
+
+  Post copyWith({
+    String? id,
+    String? title,
+    String? description,
+  }) {
+    return Post(
+      id: id ?? this.id,
+      title: title ?? this.title,
+      description: description ?? this.description,
+    );
+  }
 }
