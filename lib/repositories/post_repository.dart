3eb6935e93b0460a809fa0c35bcf162966f18c28
AladdineDev <aaldn.dev@gmail.com>
2024-06-30 12:41:08 +0200

    fix: exception

--- lib/repositories/post_repository.dart
+++ lib/repositories/post_repository.dart
@@ -11,7 +11,7 @@ class PostRepository {
     try {
       await remoteDataSource.createPost(post: post);
     } catch (e) {
-      throw const UpdatePostException();
+      throw const CreatePostException();
     }
   }
 
